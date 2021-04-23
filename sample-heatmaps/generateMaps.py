ee7c040aeeb0de6b361972c8396c5fda63cfa43f
nates linux desktop <nhapeman@appliedinvention.com>
2021-04-23 00:29:43 -0700

    Create awesome heatmaps to visualize.

--- sample-heatmaps/generateMaps.py
+++ sample-heatmaps/generateMaps.py
@@ -1,7 +1,56 @@
+"""This file contains several functions I used to generate 2d arrays to display
+in the heatmap. All values in the arrays are between 0 and 1.
+"""
+
+from PIL import Image
+import matplotlib.pyplot as plt
 import numpy as np
 import json
 
 
-t = np.zeros(10, 10)
+DATA_DIR = 'sample-heatmaps/'
 
-print(t)
+###############################
+# Functions to generate arrays.
+###############################
+
+def save_arr(arr, filename):
+     with open(f'{DATA_DIR}{filename}', 'w') as f:
+          lists = arr.tolist()
+          json.dump(lists, f, indent=4)
+
+
+def noise(w, h, filename):
+     arr = np.random.rand(w, h)
+     arr = np.around(arr, 2)
+     save_arr(arr, filename)
+
+
+def increasing_values(w, h, filename):
+     area = w * h
+     arr = (np.arange(0, area)/area).reshape(w, h)
+     arr = np.around(arr, 2)
+     save_arr(arr, filename)
+
+
+def generate_logo(upscale_factor, filename):
+     temp = np.asarray(Image.open('sample-heatmaps/logo.png'))
+     img = np.dot(temp[...,:3], [0.299, 0.587, 0.144])
+     img = img / np.max(img)
+     img = np.around(img, 2)
+     img = np.kron(img, np.ones((upscale_factor, upscale_factor)))
+     save_arr(img, filename)
+     plt.imshow(img)
+     plt.show()
+
+
+###############################
+# Generate arrays to play with.
+###############################
+
+noise(5, 5, 'noise_5x5.json')
+noise(1000, 1000, 'noise_1000x1000.json')
+
+increasing_values(10, 10, 'increasing_values_10x10.json')
+
+generate_logo(5, 'logo.json')
